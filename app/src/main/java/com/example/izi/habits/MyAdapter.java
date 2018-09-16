977b155a38e9872e187d23de00312954fd48ceca
aa <mrkarity@gmail.com>
2018-09-17 01:48:23 +0300

    add animation to habit expand collapse

--- app/src/main/java/com/example/izi/habits/MyAdapter.java
+++ app/src/main/java/com/example/izi/habits/MyAdapter.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
@@ -25,6 +26,18 @@ public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         final ConstraintLayout layout = (ConstraintLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.main_row, parent, false);
         //todo set on click listener to open the bigredbutton
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button button = layout.findViewById(R.id.button);
+                //todo smooth animation of layout - maybe with constraintlayout animations?
+                if (button.getVisibility() == View.GONE) {
+                    button.setVisibility(View.VISIBLE);
+                } else {
+                    button.setVisibility(View.GONE);
+                }
+            }
+        });
         MyViewHolder myViewHolder = new MyViewHolder(mContext, layout);
         return myViewHolder;
     }
